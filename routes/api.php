91860bab83aef4e0ac107dc84b42109befa651c9
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-19 10:23:50 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('bot', function (Request $request) {
+    logger("message request : ", $request->all());
 });
+Route::post('bot', ['as' => 'line.bot.message', 'uses' => 'GetMessageController@getmessage']);
